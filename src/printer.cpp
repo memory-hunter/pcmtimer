f599851c3e950dc91d77f3ebdf8f7a4e2a7086f5
MemoryHunter <59766692+memory-hunter@users.noreply.github.com>
2022-11-05 19:52:55 +0400

    Created printer class

---
+++ src/printer.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cpucounters.h>
+
+namespace printer {
+    void printSystemCounterStateDiff(const pcm::SystemCounterState &before, const pcm::SystemCounterState &after){
+        std::cout << "Instructions per Clock: " << getIPC(before, after) <<
+         "\nL2 cache hits: " << getL2CacheHits(before, after) <<
+         "\nL2 cache misses: " << getL2CacheMisses(before, after) <<
+         "\nL2 cache hit ratio: " << getL2CacheHitRatio(before, after) <<
+         "\nL3 cache hits: " << getL3CacheHits(before, after) <<
+         "\nL3 cache misses: " << getL3CacheMisses(before, after) <<
+         "\nL3 cache hit ratio: " << getL3CacheHitRatio(before, after) <<
+         "\nBytes read from DRAM: " << getBytesReadFromMC(before, after) << std::endl;
+    }
+}
