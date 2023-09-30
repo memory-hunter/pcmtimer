7820e0ada82fc18cab7fde2b968eaf9939e26ad0
MemoryHunter <59766692+memory-hunter@users.noreply.github.com>
2023-09-30 22:16:58 +0400

    updated plotting

--- plot/plot_data_points.py
+++ plot/plot_data_points.py
@@ -48,29 +48,51 @@ with open("test_cases.txt", "r") as f:
     l2 = np.array(l2)
     l3 = np.array(l3)
 
+fig, (ax1, ax2) = plt.subplots(1, 2)
+
 if eq == "y":
-    plt.gca().set_aspect('equal', adjustable='box')
-plt.grid()
+    # plt.gca().set_aspect('equal', adjustable='box') 
+    ax1.set_aspect('equal', adjustable='box')
+ax1.grid()
 
 if p_logn:
     print("Adjusting k, where runtime = k * " + rt + ". k = k/100")
     k = 100
-    plt.plot(x, y_logn, color='red', label='log(n)')
+    ax1.plot(x, y_logn, color='magenta', label='log(n)')
 if p_n:
-    plt.plot(x, y_n, color='green', label='n')
+    ax1.plot(x, y_n, color='magenta', label='n')
 if p_nlogn:
     print("Adjusting k, where runtime = k * " + rt + ". k = k/100")
     k = 100
-    plt.plot(x, y_nlogn, color='blue', label='nlog(n)')
+    ax1.plot(x, y_nlogn, color='magenta', label='nlog(n)')
 if p_n2:
     print("Adjusting k, where runtime = k * " + rt + ". k = k/10")
-    k= 10
-    plt.plot(x, y_n2, color="magenta", label="n^2")
-plt.scatter(n, runtime//k, color='black', label='Runtime', s=3)
-plt.legend()
-plt.show()
+    k = 10
+    ax1.plot(x, y_n2, color="magenta", label="n^2")
 
-plt.scatter(n, l2, color='red', label='L2', s=1)
-plt.scatter(n, l3, color='cyan', label='L3', s=1)
-plt.legend()
+combined_data = [(n[i], runtime[i] // k // n[i], l2[i], l3[i]) for i in range(len(n))]
+sorted_data = sorted(combined_data, key=lambda x: x[1], reverse=True)
+top_10_elements = sorted_data[:10]
+bottom_10_elements = sorted_data[-10:]
+a_list = np.array([item[0] for item in top_10_elements])
+b_list = np.array([item[1] * item[0] for item in top_10_elements])
+c_list = np.array([item[2] for item in top_10_elements])
+d_list = np.array([item[3] for item in top_10_elements])
+e_list = np.array([item[0] for item in bottom_10_elements])
+f_list = np.array([item[1] * item[0] for item in bottom_10_elements])
+g_list = np.array([item[2] for item in bottom_10_elements])
+h_list = np.array([item[3] for item in bottom_10_elements])
+
+ax1.scatter(n, runtime // k, color='grey', label='Runtime', s=1)
+ax1.scatter(a_list, b_list, color='red', label="Worst constant", s=10)
+ax1.scatter(e_list, f_list, color="green", label="Best constant", s=10)
+ax1.legend()
+
+ax2.scatter(n, l2, color='cyan', label='L2', s=1)
+ax2.scatter(n, l3, color='pink', label='L3', s=1)
+ax2.scatter(a_list, c_list, color="red", label="L2 of Worst", s=10)
+ax2.scatter(a_list, d_list, color="orange", label="L3 of Worst", s=10)
+ax2.scatter(e_list, g_list, color="blue", label="L2 of Best", s=10)
+ax2.scatter(e_list, h_list, color="green", label="L3 of Best", s=10)
+ax2.legend()
 plt.show()
