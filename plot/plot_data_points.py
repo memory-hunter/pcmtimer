5f1ea351a0206948ae46a9e3fbb48a1c1b53d86e
memory-hunter <59766692+memory-hunter@users.noreply.github.com>
2024-02-27 21:57:29 +0400

    works kinda??

--- plot/plot_data_points.py
+++ plot/plot_data_points.py
@@ -28,6 +28,11 @@ x = np.linspace(1, test_case_max_limit, test_case_max_limit)
 y_logn, y_n, y_nlogn, y_n2 = np.log(x), x, x*np.log2(x), x*x
 k = 1
 
+"""
+16691 369869500 0.440947 0.553453
+16691 392400000 0.530357 0.716444
+"""
+
 with open("test_cases.txt", "r") as f:
     lines = f.readlines()
 
@@ -48,10 +53,20 @@ with open("test_cases.txt", "r") as f:
     l2 = np.array(l2)
     l3 = np.array(l3)
 
+# split data into odd and even
+    
+n_odd = n[1::2]
+n_even = n[::2]
+runtime_odd = runtime[1::2]
+runtime_even = runtime[::2]
+l2_odd = l2[1::2]
+l2_even = l2[::2]
+l3_odd = l3[1::2]
+l3_even = l3[::2]
+
 fig, (ax1, ax2) = plt.subplots(1, 2)
 
 if eq == "y":
-    # plt.gca().set_aspect('equal', adjustable='box') 
     ax1.set_aspect('equal', adjustable='box')
 ax1.grid()
 
@@ -70,31 +85,30 @@ if p_n2:
     k = 10
     ax1.plot(x, y_n2, color="magenta", label="n^2")
 
-combined_data = [(n[i], runtime[i] / k / n[i], l2[i], l3[i]) for i in range(len(n))]
-sorted_data = sorted(combined_data, key=lambda x: x[1], reverse=True)
-top_10_elements = sorted_data[:10]
-bottom_10_elements = sorted_data[-10:]
-a_list = np.array([item[0] for item in top_10_elements])
-b_list = np.array([item[1] * item[0] for item in top_10_elements])
-c_list = np.array([item[2] for item in top_10_elements])
-d_list = np.array([item[3] for item in top_10_elements])
-e_list = np.array([item[0] for item in bottom_10_elements])
-f_list = np.array([item[1] * item[0] for item in bottom_10_elements])
-g_list = np.array([item[2] for item in bottom_10_elements])
-h_list = np.array([item[3] for item in bottom_10_elements])
+combined_data_odd = [(n_odd[i], runtime_odd[i] / n_odd[i], l2_odd[i], l3_odd[i]) for i in range(len(n_odd))]
+combined_data_even = [(n_even[i], runtime_even[i] / n_even[i], l2_even[i], l3_even[i]) for i in range(len(n_even))]
 
-print(e_list, f_list)
+a_list = np.array([item[0] for item in combined_data_odd])
+b_list = np.array([item[1] * item[0] for item in combined_data_odd])
+c_list = np.array([item[2] for item in combined_data_odd])
+d_list = np.array([item[3] for item in combined_data_odd])
 
-ax1.scatter(n, runtime / k, color='grey', label='Runtime', s=1)
-ax1.scatter(a_list, b_list, color='red', label="Worst constant", s=10)
-ax1.scatter(e_list, f_list, color="green", label="Best constant", s=10)
+e_list = np.array([item[0] for item in combined_data_even])
+f_list = np.array([item[1] * item[0] for item in combined_data_even])
+g_list = np.array([item[2] for item in combined_data_even])
+h_list = np.array([item[3] for item in combined_data_even])
+
+# ax1.scatter(a_list, b_list, color='red', label="Worst constant", s=10)
+# ax1.scatter(e_list, f_list, color="green", label="Best constant", s=10)
+ax1.scatter(a_list, b_list, color="red", s=5)
+ax1.scatter(e_list, f_list, color="orange", s=5)
 ax1.legend(loc='upper right', bbox_to_anchor=(1, 1))
 
-ax2.scatter(n, l2, color='cyan', label='L2', s=1)
-ax2.scatter(n, l3, color='pink', label='L3', s=1)
-ax2.scatter(a_list, c_list, color="red", label="L2 of Worst", s=10)
-ax2.scatter(a_list, d_list, color="orange", label="L3 of Worst", s=10)
-ax2.scatter(e_list, g_list, color="blue", label="L2 of Best", s=10)
-ax2.scatter(e_list, h_list, color="green", label="L3 of Best", s=10)
+# ax2.scatter(n, l2, color='cyan', label='L2', s=1)
+# ax2.scatter(n, l3, color='pink', label='L3', s=1)
+# ax2.scatter(a_list, c_list, color="red", label="L2 of Worst", s=10)
+# ax2.scatter(a_list, d_list, color="orange", label="L3 of Worst", s=10)
+# ax2.scatter(e_list, g_list, color="blue", label="L2 of Best", s=10)
+# ax2.scatter(e_list, h_list, color="green", label="L3 of Best", s=10)
 ax2.legend()
 plt.show()
